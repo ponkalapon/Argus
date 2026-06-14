e58f5d6afe9268c48b943f68336b4eac1c6efb1d
Dmitry <dimap@weareblah.com>
2026-06-14 18:04:03 +0300

    chore: bump CURRENT_BUILD to 3

--- apps/argus-mobile/src/services/autoUpdate.ts
+++ apps/argus-mobile/src/services/autoUpdate.ts
@@ -3,7 +3,7 @@ import { Platform, Alert } from 'react-native';
 const REPO_OWNER = 'ponkalapon';
 const REPO_NAME = 'Argus';
 const GITHUB_API = `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases/latest`;
-export const CURRENT_BUILD = 2; // increment for each release
+export const CURRENT_BUILD = 3; // increment for each release
 
 interface GitHubRelease {
   tag_name: string;
